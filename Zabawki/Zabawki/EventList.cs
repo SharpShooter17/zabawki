a5f648c713c076a6bc3e299cbddf119e18902d08
SharpShooter <b.ujazdowski@gmail.com>
2018-03-24 22:49:42 +0100

    Przeciazenie klasy List i dodanie eventu po dodaniu obiektu do listy

---
+++ Zabawki/Zabawki/EventList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zabawki
+{
+    class EventList<T> : List<T>
+    {
+        public delegate void objectAdded(T item, EventArgs eventArgs);
+        public event objectAdded AddEvent;
+
+        public new void Add(T item)
+        {
+            base.Add(item);
+            AddEvent(item, new EventArgs());
+        }
+    }
+}
--- Zabawki/Zabawki/Program.cs
+++ Zabawki/Zabawki/Program.cs
@@ -8,15 +8,21 @@ namespace Zabawki
 {
     class Program
     {
+        static void DodanoZabawke(Object item, EventArgs eventArgs)
+        {
+            Zabawka z = (Zabawka)item;
+            Console.WriteLine("Dodano zabawke do pokoju: " + z.Name);
+        }
+
         static void Main(string[] args)
         {
             PokojZabawek room = new PokojZabawek();
-
+            room.Zabawki.AddEvent += DodanoZabawke;
             Car audi = new Car("Audi A4");
             Box chocolateBox = new Box("Chocolate");
 
-            room.zabawki.Add( audi );
-            room.zabawki.Add( chocolateBox );
+            room.Zabawki.Add( audi );
+            room.Zabawki.Add( chocolateBox );
             room.pokazZabawki();
             audi.Accelerate = 5;
             chocolateBox.Dive = 3;
